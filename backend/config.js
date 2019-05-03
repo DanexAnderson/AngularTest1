02b79074a2eff90d6052014b35745c2f8bec1f51
“DanexAnderson” <daneanderson16@gmail.com>
2019-05-03 23:17:05 +0000

    beta test

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    // url: 'mongodb://localhost:27017/products',
-   url: 'mongodb+srv://danex:knox2005@cluster0-uv7hd.mongodb.net/products?retryWrites=true',
-    serverport: process.env.PORT ||  3000
+   url: 'mongodb://localhost:27017/products',
+  serverport: process.env.PORT ||  3000
 }
